bea63d5871e0df67eb20c481cf2e2f74bae3a513
Olamide Aderogba <G00401971@atu.ie>
2024-12-22 23:25:16 +0000

    Created Notification Project, updated application properties and pom file

---
+++ src/test/java/ie/atu/notificationserviceapplication/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.notificationserviceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
