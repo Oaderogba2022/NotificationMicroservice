f32591deb6249e01d53e29d7bb9f72687c9f9097
Olamide Aderogba <G00401971@atu.ie>
2024-12-24 20:00:38 +0000

    Updated Notification Controller and Service Class.

--- src/main/java/ie/atu/notificationserviceapplication/NotificationController.java
+++ src/main/java/ie/atu/notificationserviceapplication/NotificationController.java
@@ -1,49 +1,40 @@
 package ie.atu.notificationserviceapplication;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/notifications")
-@RequiredArgsConstructor
 public class NotificationController {
 
-    private final NotificationService notificationService;
+    @Autowired
+    private NotificationService notificationService;
 
     @PostMapping
-    public ResponseEntity<Notification> createNotification(@RequestBody Notification notification) {
-        Notification createdNotification = notificationService.createNotification(notification);
-        return new ResponseEntity<>(createdNotification, HttpStatus.CREATED);
+    public Notification createNotification(@RequestBody Notification notification) {
+        // Call the service to create the notification and return the full object (including id)
+        return notificationService.createNotification(notification);
     }
 
-    @GetMapping("/{notificationId}")
-    public ResponseEntity<Notification> getNotification(@PathVariable String notificationId) {
-        Notification notification = notificationService.getNotificationById(notificationId);
-        return notification != null ? new ResponseEntity<>(notification, HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @PutMapping("/{notificationId}")
-    public ResponseEntity<Notification> updateNotification(@PathVariable String notificationId, @RequestBody Notification notification) {
-        Notification updatedNotification = notificationService.updateNotification(notificationId, notification);
-        return updatedNotification != null ? new ResponseEntity<>(updatedNotification, HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @DeleteMapping("/{notificationId}")
-    public ResponseEntity<Void> deleteNotification(@PathVariable String notificationId) {
-        boolean isDeleted = notificationService.deleteNotification(notificationId);
-        return isDeleted ? new ResponseEntity<>(HttpStatus.NO_CONTENT) :
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/{id}")
+    public Optional<Notification> getNotificationById(@PathVariable String id) {
+        return notificationService.getNotificationById(id);
     }
 
     @GetMapping
-    public ResponseEntity<List<Notification>> getAllNotifications() {
-        List<Notification> notifications = notificationService.getAllNotifications();
-        return new ResponseEntity<>(notifications, HttpStatus.OK);
+    public List<Notification> getAllNotifications() {
+        return notificationService.getAllNotifications();
+    }
+
+    @PutMapping("/{id}")
+    public Notification updateNotification(@PathVariable String id, @RequestBody Notification notification) {
+        return notificationService.updateNotification(id, notification);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNotification(@PathVariable String id) {
+        notificationService.deleteNotification(id);
     }
 }
