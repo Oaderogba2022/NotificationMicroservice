59bc2e12303c6651ba33bcca491776ce0b6c140a
Olamide Aderogba <G00401971@atu.ie>
2025-01-03 11:06:00 +0000

    Removed Rabbit Config Classes

--- src/main/java/ie/atu/notificationserviceapplication/NotificationService.java
+++ src/main/java/ie/atu/notificationserviceapplication/NotificationService.java
@@ -1,7 +1,6 @@
 package ie.atu.notificationserviceapplication;
 
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +10,8 @@ import java.util.List;
 @Service
 public class NotificationService {
 
-    private final NotificationRepository notificationRepository;
-    private final RabbitTemplate rabbitTemplate;
-
     @Autowired
-    public NotificationService(NotificationRepository notificationRepository, RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.notificationRepository = notificationRepository;
-    }
-
+    private NotificationRepository notificationRepository;
 
     public List<Notification> getAllNotifications() {
         return notificationRepository.findAll();
@@ -45,15 +37,7 @@ public class NotificationService {
     }
 
     public Notification sendNotification(Notification notification) {
-        sendNotificationToQueue(notification);
         notification.setSent(true);
         return notificationRepository.save(notification);
     }
-
-    // Private method to encapsulate RabbitMQ messaging logic
-    private void sendNotificationToQueue(Notification notification) {
-        rabbitTemplate.convertAndSend("notificationQueue", notification);
-        System.out.println("Sent Notification to Queue: " + notification);
-    }
-
 }
